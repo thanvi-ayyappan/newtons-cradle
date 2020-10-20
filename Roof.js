40d40a57d2cd11d8900d22660da1f6fd3c5ba8c8
thanvi-ayyappan <70520850+thanvi-ayyappan@users.noreply.github.com>
2020-10-20 10:50:56 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,17 @@
+class Roof{
+    constructor(x,y,width,height){
+      var opt={
+        isStatic:true
+      }
+        this.body=Bodies.rectangle(x,y,width,height,opt);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+          }
+          display(){
+            var pos=this.body.position;
+          rectMode(CENTER);
+          fill("white");
+          rect(pos.x,pos.y,this.width,this.height);
+          }
+      }
